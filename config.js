4b9c54015439225b541e8d74f412730b16522592
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-04-17 23:06:34 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '706005003836-ntm84to7ulgdf3i884g8anrvi7rnnho3.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'bH0raFBpLaR9l6QK3hmwPT-N',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
